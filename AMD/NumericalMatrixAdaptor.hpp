ac7de5110fa69decf24525cf10f7e0941a8eb107
suyang zhu <timberlandbear@gmail.com>
2014-06-02 11:49:15 -0400

    add NumericalMatrixAdaptor.hpp

---
+++ AMD/NumericalMatrixAdaptor.hpp
@@ -0,0 +1,16 @@
+#ifndef AMD_NumericalMatrixAdaptor_HPP
+#define AMD_NumericalMatrixAdaptor_HPP
+/*
+ * @file NumbericalMatrixAdaptor
+ *
+ * @author Suyang Zhu and Anju Kambadur
+ *
+ * This file defines the adaptor for numerical matrix object. 
+ */
+
+#include "MatrixAdaptor.hpp"
+
+namespace AMD {
+}
+
+#endif
