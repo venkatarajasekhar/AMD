a9a720b4b9c3e38deb8ab20d9abe07eef7fd8bac
Gabriel Borges <gabriel@gabrielborg.es>
2014-10-20 11:55:14 -0200

    Adding other methods to the SymbolicScalarMatlab python binding. Still left to be done: copy and (friend) operators.

--- AMD/python/SymbolicScalarMatlab.cpp
+++ AMD/python/SymbolicScalarMatlab.cpp
@@ -10,6 +10,12 @@
 
 namespace AMD { namespace python {
 
+/* The two macro overloads following are necessary to expand the depicted *
+ * methods with all the possibilities for their default arguments, once *
+ * python can't see that by default */
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(print_overloads, print, 0, 1)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(println_overloads, println, 0, 1)
+
 /**
  * A function to export all the definitions for context object.
  */
@@ -18,8 +24,15 @@ void export_symbolicscalarmatlab () {
   using namespace boost::python;
   using namespace AMD;
 
-  class_<SymbolicScalarMatlab>("symScalMat", init<double>())
-    .def("print", &SymbolicScalarMatlab::print)
+  class_<SymbolicScalarMatlab>("symScalMat", init<>())
+  	.def(init<std::string>())
+    .def(init<double>())
+  	/* Attention to the capital 'P' in Print, such as *
+  	 * to avoid conflict with python built-in 'print' */
+  	.def("Print", &SymbolicScalarMatlab::print, print_overloads())
+  	.def("println", &SymbolicScalarMatlab::println, println_overloads())
+    //.def("copy", SymbolicScalarMatlab::copy)
+    .def("getString", &SymbolicScalarMatlab::getString)
   ;
 
 }
