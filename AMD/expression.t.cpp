568e9ab42daf4d1d7f26bacba68843366e794362
Anju Kambadur <prabhanjankambadur@gmail.com>
2015-02-01 18:35:03 -0500

    Adding more tests that fail

--- AMD/expression.t.cpp
+++ AMD/expression.t.cpp
@@ -7,12 +7,23 @@ BOOST_AUTO_TEST_CASE ( generateExpression )
 {
     AMD::Expression testExpression;
 
-    // This should generate an exception as we do not implement A''. When
+    // This should generate an exception as we do not implement A'_. When
     // we fix this, we will no longer fail this test
     BOOST_CHECK_NO_THROW ((testExpression = AMD::generateExpression("A'_")));
 
-    // This should generate an exception as we do not implement A''. When
+    // This should generate an exception as we do not implement A_'. When
     // we fix this, we will no longer fail this test
     BOOST_CHECK_NO_THROW ((testExpression = AMD::generateExpression("A_'")));
 
+    // This should generate an exception as we do not implement A o B. When
+    // we fix this, we will no longer fail this test
+    BOOST_CHECK_NO_THROW ((testExpression = AMD::generateExpression("A o B")));
+
+    // This should generate an exception as we do not implement tr(A). When
+    // we fix this, we will no longer fail this test
+    BOOST_CHECK_NO_THROW ((testExpression = AMD::generateExpression("tr(A)")));
+    
+    // This should generate an exception as we do not implement lgdt(A). When
+    // we fix this, we will no longer fail this test
+    BOOST_CHECK_NO_THROW ((testExpression=AMD::generateExpression("lgdt(A)")));
 }
