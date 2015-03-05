b06dc34efa6a78d20b1b4e5078634cd5b195b8a1
Chetan Gupta <cpgupta561@gmail.com>
2015-03-05 12:43:36 -0500

    added initial documentation for test

--- AMD/expression.t.cpp
+++ AMD/expression.t.cpp
@@ -3,6 +3,7 @@
 #define BOOST_TEST_MODULE ExpressionTest
 #include <boost/test/unit_test.hpp>
 
+//Do we still need this if we have the regression test?
 BOOST_AUTO_TEST_CASE ( generateExpression )
 {
     AMD::Expression testExpression;
@@ -29,31 +30,54 @@ BOOST_AUTO_TEST_CASE ( generateExpression )
     BOOST_CHECK_NO_THROW ((testExpression=AMD::generateExpression("lgdt(A)")));
 }
 
+
+/*  Regression test that checks whether the parser is functioning correctly
+ *  compareExpectedExpressions takes in two input strings which represent
+ *  the input to parse followed by the true parsed string to compare against
+ */
+
 BOOST_AUTO_TEST_CASE ( parseExpression )
 { 
+    
+    //single matrix test
     BOOST_CHECK(AMD::compareExpectedExpressions("A", "\"A\"") == 1);
+    
+    //binary op expressions
     BOOST_CHECK(AMD::compareExpectedExpressions("A+B", "(+ \"A\" \"B\")")); 
     BOOST_CHECK(AMD::compareExpectedExpressions("A-B", "(- \"A\" \"B\")")); 
     BOOST_CHECK(AMD::compareExpectedExpressions("A*B", "(* \"A\" \"B\")")); 
+    BOOST_CHECK(AMD::compareExpectedExpressions("AoB", "(o \"A\" \"B\")")); 
+    BOOST_CHECK(AMD::compareExpectedExpressions("A/B", "(/ \"A\" \"B\")")); 
+    
+    //multiple expressions
     BOOST_CHECK(AMD::compareExpectedExpressions("A-B+C", 
             "(+ (- \"A\" \"B\") \"C\")")); 
-    BOOST_CHECK(AMD::compareExpectedExpressions("AoB", "(o \"A\" \"B\")")); 
+    
+    //unary op expressions
     BOOST_CHECK(AMD::compareExpectedExpressions("tr(B)", "(tr \"B\")"));     
     BOOST_CHECK(AMD::compareExpectedExpressions("lgdt(B)", "(lgdt \"B\")"));     
     BOOST_CHECK(AMD::compareExpectedExpressions("B'", "(' \"B\")"));     
     BOOST_CHECK(AMD::compareExpectedExpressions("B_", "(_ \"B\")"));     
+   
+
+    //tricky unary op combinations
+    BOOST_CHECK(AMD::compareExpectedExpressions("-B'", "(- (' \"B\"))") == 1);     
+    BOOST_CHECK(AMD::compareExpectedExpressions("B''", "(' (' \"B\"))"));     
+    BOOST_CHECK(AMD::compareExpectedExpressions("-B'_", "(- (_ (' \"B\")))"));     
+   
+    
+    //combination of binary and unary op expressions
+    BOOST_CHECK(AMD::compareExpectedExpressions("A'+B'", 
+            "(+ (' \"A\") (' \"B\"))"));     
     BOOST_CHECK(AMD::compareExpectedExpressions("A+B'", "(+ \"A\" (' \"B\"))"));     
     BOOST_CHECK(AMD::compareExpectedExpressions("A'+B'", 
             "(+ (' \"A\") (' \"B\"))"));     
-    BOOST_CHECK(AMD::compareExpectedExpressions("A'+B'", 
-            "(+ (' \"A\") (' \"B\"))"));     
-    BOOST_CHECK(AMD::compareExpectedExpressions("-B'", "(- (' \"B\"))") == 1);     
     BOOST_CHECK(AMD::compareExpectedExpressions("-A'/-B'", 
             "(/ (- (\' \"A\")) (- (\' \"B\")))"));     
+    
+    //complex tr expressions
     BOOST_CHECK(AMD::compareExpectedExpressions("tr(A)*tr(B)", 
             "(* (tr \"A\") (tr \"B\"))")); 
     BOOST_CHECK(AMD::compareExpectedExpressions("tr(A)*tr(B)+7*4*A", 
             "(+ (* (tr \"A\") (tr \"B\")) (* (* \"7\" \"4\") \"A\"))"));  
-    BOOST_CHECK(AMD::compareExpectedExpressions("B''", "(' (' \"B\"))"));     
-    BOOST_CHECK(AMD::compareExpectedExpressions("-B'_", "(- (_ (' \"B\")))"));     
 } 
